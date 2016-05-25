a24ab172d7535fba0027f4a8e1ada1e962582018
rpoplawski <radoslaw.poplawski@gazeta.pl>
2016-05-25 16:50:40 +0200

    commit

--- game/main.js
+++ game/main.js
@@ -2,12 +2,15 @@
 'use strict';
 
 var gameSpeed = 400;
-var dir = 2;
+var dir = 0;
 var $length;
+var route = [];
 var possibleStarts = [[4,4],[10,4],[15,7],[4,14],[6,11]];
 var possibleEnds = [[16,44],[12,24],[15,27],[13,28]];
 var $game = $('#game');
-var metroOnMap = false;
+var $controls = $('#controls');
+var remainTracks = 30;
+var startPoint, endPoint, metroDir, newMetrox, newMetroy;
 
 function createBoard() {
     for (var i=1; i<26; i++) {
@@ -19,99 +22,132 @@ function createBoard() {
     }
 }
 function createControls() {
-    for (var i=1; i<6; i++) {
+    for (var i=1; i<5; i++) {
         for (var j=1; j<21; j++) {
             var $div = $('<div>');
-            $('#controls').append($div);
+            $controls.append($div);
             $div.addClass('cell').attr('id','controlcell-'+i+'-'+j);
         }
     }
-    $('#controlcell-3-3').addClass('railroad');
-    $('#controlcell-3-13').addClass('metro');
+    $('#controlcell-1-4').addClass('railroad');
+    $('#controlcell-1-10').addClass('bulldozer');
+    $('#controlcell-3-4').text('Buduj szlak');
+    $('#controlcell-3-10').text('Kasuj');
+    $('#controlcell-2-16').append('<button>Start!</button>');
+    $('button').attr('id','startButton')
+    $('#counter').text(remainTracks);
 }
 
 function createPlaces() {
-    var startPoint = possibleStarts[Math.round(Math.random()*4)];
-    var endPoint = possibleEnds[Math.round(Math.random()*3)];
+    startPoint = possibleStarts[Math.round(Math.random()*4)];
+    endPoint = possibleEnds[Math.round(Math.random()*3)];
     $('#cell-'+startPoint[0]+'-'+startPoint[1]).addClass('start-point');
     $('#cell-'+endPoint[0]+'-'+endPoint[1]).addClass('end-point');
 }
 function buildRoads() {
     $game.off();
-    $game.on('click','.cell', function () {
-        $(this).addClass('railroad');
+    $game.on('click','.cell:not(.railroad)', function () {
+        if ($('.railroad').length < 31) {
+            $(this).addClass('railroad');
+            route.push($(this).attr('id'));
+            remainTracks -=1;
+            $('#counter').text(remainTracks);
+        }
     });
 }
 function buildMetro() {
+    $('#cell-'+startPoint[0]+'-'+startPoint[1]).addClass('metro');
+    return metroDir = startPoint;
+}
+function bulldoze() {
     $game.off();
-    if (metroOnMap === false) {
-        $game.on('click', '.cell', function () {
-            $(this).addClass('metro');
-            metroOnMap = true;
-        });
-    }
+    $game.on('click','.railroad', function () {
+            $(this).removeClass('railroad');
+    });
 }
 
-// $('#controls').on('click','div#controlcell-3-3', buildRoads());
-$('#controls').on('click','#controlcell-3-13', buildMetro());
+function checkRoute(position) {
+    return route.some(function(a) {
+        return 'cell-'+position[0]+'-'+position[1] === a;
+    })
+}
 
-/*function gameplay() {
-    var newx = snake[0][0];
-    var newy = snake[0][1];
-    var $food = $('.food');
+function startButton() {
+    $controls.on('click','#startButton', function() {
+        $('#controlcell-1-4').removeClass('railroad');
+        $('#controlcell-1-10').removeClass('bulldozer');
+        $('#controls').off();
+        $('#controlcell-2-16').empty();
+        buildMetro();
+        $(document).keydown(function(e){
+            if (e.keyCode == 37) {
+                dir=4;
+            }else if (e.keyCode == 38) {
+                dir=1;
+            }else if (e.keyCode == 39) {
+                dir=2;
+            }else if (e.keyCode == 40) {
+                dir=3;
+            }
+            moveMetro();
+        });
+    });
+}
+function moveMetro() {
+    $game.off();
+    newMetrox = metroDir[0];
+    newMetroy = metroDir[1];
+
     switch (dir) {
         case 1:
-            newx -= 1;
+            newMetrox -= 1;
             break;
         case 2:
-            newy += 1;
+            newMetroy += 1;
             break;
         case 3:
-            newx += 1;
+            newMetrox += 1;
             break;
         case 4:
-            newy -= 1;
+            newMetroy -= 1;
             break;
     }
-    if ((newx < 1) || (newy < 1) || (newx > 20) || (newy > 20) || ($('#cell-' + newx + '-' + newy).hasClass('snake'))) {
-        $('.cell').addClass('black');
-    } else if ($food.attr('id') === 'cell-' + newx + '-' + newy) {
-        snake.unshift([newx, newy]);
-        $('#cell-' + newx + '-' + newy).addClass('snake').removeClass('food');
-        if ($length === 399) {
-            setTimeout(function () {$('.cell').removeClass('snake');}, 1000);
-        } else {
-            createFood();
-            gameSpeed -= 1;
-            setTimeout(function () {gameplay();}, gameSpeed);}
+
+    $('#cell-'+newMetrox+'-'+newMetroy).addClass('metro');
+    $('#cell-'+metroDir[0]+'-'+metroDir[1]).removeClass('metro');
+    metroDir = [newMetrox,newMetroy];
+    if ((metroDir[0] === endPoint[0]) && (metroDir[1] === endPoint[1])) {
+        console.log('WIN');
+        $(document).off('keydown');
+        $game.empty();
+        $controls.empty();
+        createBoard();
+        createPlaces();
+        createControls();
+        startButton();
+    } else if (checkRoute(metroDir) === true) {
+        var index = route.indexOf('cell-'+metroDir[0]+'-'+metroDir[1]);
+        delete route[index];
     } else {
-        var last = snake.pop();
-        snake.unshift([newx,newy]);
-        $('#cell-'+last[0]+'-'+last[1]).removeClass('snake');
-        $('#cell-'+newx+'-'+newy).addClass('snake');
-        setTimeout(function() {gameplay();}, gameSpeed);
+        console.log('LOOSER');
+        $(document).off('keydown');
+        $game.empty();
+        $controls.empty();
+        createBoard();
+        createPlaces();
+        createControls();
+        startButton();
     }
+
 }
+createBoard();
+createPlaces();
+createControls();
 
-function game() {
-    createBoard();
-    createFood();
-    setTimeout(function() {gameplay();}, gameSpeed);
-    $(document).keydown(function(e){
-        if (e.keyCode == 37) {
-            dir=4;
-        }else if (e.keyCode == 38) {
-            dir=1;
-        }else if (e.keyCode == 39) {
-            dir=2;
-        }else if (e.keyCode == 40) {
-            dir=3;
-        }
-    });
-}*/
-
-$(document).ready(function() {
-    createBoard();
-    createPlaces();
-    createControls();
+$controls.on('click','#controlcell-1-4', function() {
+    buildRoads();
 });
+$controls.on('click','#controlcell-1-10', function() {
+    bulldoze();
+});
+startButton();
