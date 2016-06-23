3a19ba999268c3f10bc5d21d01b2c9182adf4e76
Radek <radoslaw.poplawski@gazeta.pl>
2016-06-23 23:22:39 +0200

    commit weather API

--- dijkstra/main.js
+++ dijkstra/main.js
@@ -1,37 +1,50 @@
 'use strict';
 var places = [
     {
+        id: 0,
         name: 'A',
-        siblings: ['B','C']
+        siblings: [1,2]
     },
     {
+        id: 1,
         name: 'B',
-        siblings: ['A','D']
+        siblings: [0,3]
     },
     {
+        id: 2,
         name: 'C',
-        siblings: ['A']
+        siblings: [0]
     },
     {
+        id: 3,
         name: 'D',
-        siblings: ['B']
+        siblings: [1,4]
+    },
+    {
+        id: 4,
+        name: 'E',
+        siblings: [3]
     }
 ];
-var route = [];
-var start = places[0];
-var end = places[3];
+var i = 0;
+var route = [{id: 0, moves: [0]}];
+var end = places[4];
 var foundEnd = false;
-var point;
-var object = {};
-object.name = 'A';
-object.moves = ['A'];
 
 while (foundEnd === false) {
-    point = point || start;
-    point.siblings.forEach(function(sibling) {
-        object.name = sibling;
-        object.moves.push(sibling);
-        route.push(object);
-    })
+    var idPoint = route[i].id;
+    places[idPoint].siblings.forEach(function(sibling) {
+        if (route[i].moves.some(function(item) {return sibling === item}) === false) {
+            var object = {};
+            object.id = sibling;
+            object.moves = route[i].moves.slice();
+            object.moves.push(sibling);
+            route.push(object);
+            console.log(JSON.stringify(route));
+            if (object.id === end.id) {foundEnd = true;}
+        }
+    });
 
+    i++;
 }
+console.log(route[route.length-1].moves);
---
+++ weather API/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $("form").on('submit', function (event) {
+        event.preventDefault();
+        var options = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://api.openweathermap.org/data/2.5/weather?q="+$('#city').val()+"&units=metric&APPID=950b6c460cea43799e2dcaec91465e73",
+            "method": "GET"
+        };
+
+        $.ajax(options)
+            .done(function (result) {
+                showResponse(result);
+            })
+            .fail(function (error) {
+                console.error(error);
+            });
+
+        function showResponse(object) {
+            if (object.cod === '404') {
+                $('#info').append($('<div>').text('Miasto nie znalezione')).append($('<br>'));
+            } else {
+                $('#info').append($('<div>').text('Miasto: ' + object.name))
+                    .append($('<div>').text('Id: ' + object.id))
+                        .append($('<div>').text('N: ' + object.coord.lat + ', E:' + object.coord.lon))
+                            .append($('<div>').text('Temp: ' + object.main.temp + '^C'))
+                                .append($('<br>'));
+            }
+        }
+    });
+});
